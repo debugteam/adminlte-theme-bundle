2cd70fb5abfe33d445c38912475d21cfe911be06
Jochen Schultz <jschultz@php.net>
2022-10-08 06:16:15 +0200

    fiddeling on psr

--- src/AdminLTE3ThemeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AdminLTE3ThemeBundle extends Bundle
-{
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
-}
---
+++ src/DebugteamAdminLTEThemeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Debugteam\AdminLTEThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class DebugteamAdminLTEThemeBundle extends AbstractBundle
+{
+}
